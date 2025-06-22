e552b2cc456d3b4be66dbc1d29f74ad629cf1d25
RichardPolivka <147742779+RichardPolivka@users.noreply.github.com>
2025-06-22 16:24:28 -0700

    stuff fuck work

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -2,6 +2,16 @@ extends CharacterBody2D
 
 @export var speed = 600
 
+
+@onready var sprite = $Sprite2D # Assuming the sprite is a child of the CharacterBody2D
+
+func _on_area_2d_area_entered(area):
+
+	if area.name == "changer": # Replace "Area2DName" with the actual name of your Area2D
+		# Load a new texture (replace "new_sprite.png" with your desired texture)
+		var new_texture = load("res://download.png")
+		sprite.texture = new_texture
+
 func get_input():
 	var input_direction = Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
 	velocity = input_direction * speed
